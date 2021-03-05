5969b476492ef727b4811039923ec6afa4d37f63
Арина <78840926+ar1shka@users.noreply.github.com>
2021-03-05 14:47:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,9 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b){while ( a!=b)
-if (a > b) a -= b;
-else b -= a;return a;}
+int gcd(int a, int b){
+while(a!=b)
+if (a > b)
+a -= b;
+else b -= a;
+return a;
+}
